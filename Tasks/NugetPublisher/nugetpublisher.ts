352c04644c736c272c31962559b5975e84d6bbd9
t-mawill <t-mawill@microsoft.com>
2016-08-31 18:36:22 -0700

    Update nugetpublisher.ts

--- Tasks/NugetPublisher/nugetpublisher.ts
+++ Tasks/NugetPublisher/nugetpublisher.ts
@@ -40,11 +40,11 @@ async function main(): Promise<void> {
 
 
         //read inputs
-        var solution = tl.getPathInput('solution', true, false);
-        var filesList = nutil.resolveFilterSpec(solution, tl.getVariable('System.DefaultWorkingDirectory') || process.cwd());
-        filesList.forEach(solutionFile => {
-            if (!tl.stats(solutionFile).isFile()) {
-                throw new Error(tl.loc('NotARegularFile', solutionFile));
+        var searchPattern = tl.getPathInput('searchPattern', true, false);
+        var filesList = nutil.resolveFilterSpec(searchPattern, tl.getVariable('System.DefaultWorkingDirectory') || process.cwd());
+        filesList.forEach(packageFile => {
+            if (!tl.stats(packageFile).isFile()) {
+                throw new Error(tl.loc('NotARegularFile', packageFile));
             }
         });
         var connectedServiceName = tl.getInput('connectedServiceName');
